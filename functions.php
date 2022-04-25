7b6f37a83ceab256406d6a59c3560dc96adb338f
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-04-26 00:03:34 +0200

    v0.1.1
    Edit and comment loadInventory function.

--- functions.php
+++ functions.php
@@ -2,9 +2,13 @@
 
 function loadInventory(string $steamId)
 {
-    $data = json_decode(file_get_contents("https://steamcommunity.com/id/{$steamId}/inventory/json/252490/2?l=english&count=5000"), true);
-    $inventory = $data["rgInventory"];
-    $descriptions = $data["rgDescriptions"];
+    // GET and output source code of the website
+    $response = file_get_contents("https://steamcommunity.com/id/{$steamId}/inventory/json/252490/2?l=english&count=5000");
+    // Decode JSON data into a PHP associative array
+    $data = json_decode($response, true);
+    // Use array destructuring to assign rgInventory and rgDescriptions from $data to two variables.
+    ['rgInventory' => $inventory, 'rgDescriptions' => $descriptions] = $data;
+
     echo ("Fetching your Rust inventory.");
 
     $counts = [];
